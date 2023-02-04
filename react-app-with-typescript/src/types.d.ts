c30c9c5918e1fa611310ffd5544e490ddaaab949
EduRamos95 <eduramos08.30@gmail.com>
2023-02-04 12:07:16 -0500

    app.v7.2 - create file to interface definitions and re-use

---
+++ react-app-with-typescript/src/types.d.ts
@@ -0,0 +1,9 @@
+// types.d.ts -----------------------------
+// ----- solo puede tener definiciones ----
+
+export interface Sub{
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}

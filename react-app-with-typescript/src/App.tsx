5d82ce3b8c943940e30e7ac9f6a619b9bb767b76
EduRamos95 <eduramos08.30@gmail.com>
2023-02-03 20:20:06 -0500

    app.v2 - create a interface without data

--- react-app-with-typescript/src/App.tsx
+++ react-app-with-typescript/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
+// crear interface ----------
+interface Sub {
+  nick: string
+  avatar: string
+  subMonths: number
+  description?: string
+}
+// --------------------------
 function App() {
+  // colocamos la interface como un tipo de dato que recibira
+  const [subs, setSubs] = useState<Array<Sub>>([])
+  // ahora no hay error, simplemente los datos estan vacios
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>edu subs </h1>
+      <ul>
+        {
+          subs.map(sub => {
+            return (
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt={`Avatar for ${sub.nick}`}/>
+                <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0, 100)}</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
